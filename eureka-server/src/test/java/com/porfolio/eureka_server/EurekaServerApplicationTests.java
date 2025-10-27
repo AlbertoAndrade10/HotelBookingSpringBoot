42445c9060006fb492ca376a3a84ced78e1221b3
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-10-27 17:42:01 +0100

    first commit

---
+++ eureka-server/src/test/java/com/porfolio/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
